44bb1afabcdda9cf25d2615315229c7a45b060fa
CodeDeivid <deividmickael@gmail.com>
2024-02-25 11:27:44 -0300

    bug fixes

--- src/components/Cardproject.jsx
+++ src/components/Cardproject.jsx
@@ -13,7 +13,7 @@ function Cardproject(props) {
         <div className="buttons-proj hidden max-2xl:flex items-center justify-center gap-2 mt-4">
           <a href={`${props.details}`} target="_blank" className="card-project-button flex flex-row-reverse items-center rounded-xl bg-white text-zinc-950 border-[1px] border-zinc-700 p-3 w-full hover:bg-transparent hover:text-white transition-all justify-center font-semibold duration-700 gap-2 group/edit">
             Detalhes
-            <img src="./src/assets/details.png" alt="Details icon" className="brightness-0 w-3 transition-all duration-700 group/edit"/>
+            <img src="/assets/details.png" alt="Details icon" className="brightness-0 w-3 transition-all duration-700 group/edit"/>
           </a>
           <a href={`${props.github}`} target="_blank" className="flex flex-row-reverse items-center rounded-xl bg-zinc-500 border-[1px] border-zinc-700 p-3 gap-2 w-full hover:brightness-150 transition-all justify-center duration-300 group/edit" id="email-button">
             GitHub
